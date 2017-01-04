35d7f7742fa62ac27a0ff4b967920a789cb18b83
cem23 <cem23@uw.edu>
2017-01-04 12:36:34 -0800

    Adding in project files.

---
+++ Contoso01/Contoso01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Contoso01.Startup))]
+namespace Contoso01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
